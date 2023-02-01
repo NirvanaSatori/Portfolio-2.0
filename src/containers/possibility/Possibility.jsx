e1b0aaf47a1265d8c4f5d4cc5e7b4e4cf6f783a7
MORRIS <morris24kariuki@gmail.com>
2023-02-01 21:37:01 +0300

    added essential folders and files

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './possibilty.css'
+
+function Possibility() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Possibility
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatGPT3.css'
+
+function whatGPT3() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default whatGPT3
