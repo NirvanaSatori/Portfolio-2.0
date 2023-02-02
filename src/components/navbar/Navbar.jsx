9017f9599a6fe90fa007bde8eb933990c5e23692
MORRIS <morris24kariuki@gmail.com>
2023-02-02 21:29:46 +0300

    input content and styling to article component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,11 +10,12 @@ function Navbar() {
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'></div>
-        <img src={logo} alt="logo"/>
+        {/* <img src={logo} alt="logo"/> */}
+        <h1>Morris GoldKey</h1>
       </div>
       <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#wgpt3">Who is GoldKey?</a></p>
           <p><a href="#possibility">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
@@ -31,7 +32,7 @@ function Navbar() {
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
             <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#wgpt3">Who is GoldKey?</a></p>
             <p><a href="#possibility">Open AI</a></p>
             <p><a href="#features">Case Studies</a></p>
             <p><a href="#blog">Library</a></p>
