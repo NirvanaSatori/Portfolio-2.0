d7957ed9bcbbc012fbafaee9d9a9c83d18f45afc
MORRIS <morris24kariuki@gmail.com>
2023-02-02 21:52:48 +0300

    added the footer links and content

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,9 +3,45 @@ import './footer.css'
 
 function Footer() {
   return (
-    <div>
-      
+    <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
     </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>Morris GoldKey, <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Morris GoldKey</p>
+        <p>+254716135259</p>
+        <p>morris.waithaka42@gmail.com</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2023 GoldKey. All rights reserved.</p>
+    </div>
+  </div>
   )
 }
 
